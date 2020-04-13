5a77d84d64335c2247cc50abfae88bab86024660
bartimaeusrex <mrpatrickreinhart@gmail.com>
2020-04-12 22:43:36 -0400

    Data appears in the browser!

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ const app = express()
 
 app.use(express.static('public'))
 app.set('view engine', 'pug')
+// app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (request, response) => {
-  return response.render('index')
+  return response.render('index', { showdata })
+})
+
+app.get('/season/:number', (request, response) => {
+  const { number } = request.params
+
+  return response.render('season', { number })
 })
 
 app.all('*', (request, response) => {
-  return response.sendStatus(404)
+  return response.sendStatus(404).send('Hello! Oh no!')
 })
 
 app.listen(8080, () => {
+  // eslint-disable-next-line no-console
   console.log('Listening on port 8080.')
+})
