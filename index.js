51a272d113f8b476c0eb20ede34a9dd88ff716fc
bartimaeusrex <mrpatrickreinhart@gmail.com>
2020-04-13 21:02:16 -0400

    Everything working as it should

--- index.js
+++ index.js
@@ -9,18 +9,6 @@ app.get('/', (request, response) => {
   return response.render('index', { showdata })
 })
 
-// app.get('/season/:number', (request, response) => {
-//   const { number } = request.params
-
-//   return response.render('season', { number })
-// })
-
-// app.get('/showtimes/:id', (request, response) => {
-//   const movie = movieTimes.find((movie) => movie.id === parseInt(request.params.id))
-
-//   return response.render('showtimes', { movie })
-// })
-
 app.get('/season/:id', (request, response) => {
   const { id } = request.params
   const season = showdata.seasons.find((season) => season.number === Number(id))
@@ -28,14 +16,8 @@ app.get('/season/:id', (request, response) => {
   return response.render('season', { season, showdata })
 })
 
-// app.get('/season/:number', (request, response) => {
-//   const season = showdata.find((season) => season.number === Number(request.params.number))
-
-//   return response.render('season', { season })
-// })
-
 app.all('*', (request, response) => {
-  return response.sendStatus(404).send('Hello! Oh no!')
+  return response.sendStatus(404).send('Oh no! Not found!')
 })
 
 app.listen(8080, () => {
