6214ddaadeef2e0f05f65d90b46bb44f995dd49b
bartimaeusrex <mrpatrickreinhart@gmail.com>
2020-04-13 20:37:34 -0400

    Season pages showing content

--- index.js
+++ index.js
@@ -4,17 +4,35 @@ const app = express()
 
 app.use(express.static('public'))
 app.set('view engine', 'pug')
-// app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (request, response) => {
   return response.render('index', { showdata })
 })
 
-app.get('/season/:number', (request, response) => {
-  const { number } = request.params
+// app.get('/season/:number', (request, response) => {
+//   const { number } = request.params
 
-  return response.render('season', { number })
+//   return response.render('season', { number })
+// })
+
+// app.get('/showtimes/:id', (request, response) => {
+//   const movie = movieTimes.find((movie) => movie.id === parseInt(request.params.id))
+
+//   return response.render('showtimes', { movie })
+// })
+
+app.get('/season/:id', (request, response) => {
+  const { id } = request.params
+  const season = showdata.seasons.find((season) => season.number === Number(id))
+
+  return response.render('season', { season, showdata })
 })
+
+// app.get('/season/:number', (request, response) => {
+//   const season = showdata.find((season) => season.number === Number(request.params.number))
+
+//   return response.render('season', { season })
+// })
 
 app.all('*', (request, response) => {
   return response.sendStatus(404).send('Hello! Oh no!')
